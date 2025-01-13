b43a15767c07179db568f1a924fa0c7ac2918ff9
Harsh <harshverma7k@gmail.com>
2025-01-13 21:40:28 +0530

    worked on frontend components

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // this will catch all files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
